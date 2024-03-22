31f8cdb232153f2a0620437987939407704a36b5
Wolfgang Pendergass <119448680+AdrianWolfP@users.noreply.github.com>
2024-03-22 01:08:26 -0500

    pep8 fixing

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -6,13 +6,17 @@ Requirements:
 Records all tasks from all employees
 Format must be: {
     "USER_ID": [
-        {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS},
-        {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS},
+        {"username": "USERNAME", "task": "TASK_TITLE", 
+        "completed": TASK_COMPLETED_STATUS},
+        {"username": "USERNAME", "task": "TASK_TITLE", 
+        "completed": TASK_COMPLETED_STATUS},
         ...
     ],
     "USER_ID": [
-        {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS},
-        {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS},
+        {"username": "USERNAME", "task": "TASK_TITLE", 
+        "completed": TASK_COMPLETED_STATUS},
+        {"username": "USERNAME", "task": "TASK_TITLE", 
+        "completed": TASK_COMPLETED_STATUS},
         ...
     ]
 }
